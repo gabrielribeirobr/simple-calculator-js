81dcee3a4e6c27265b90161611ebb03aaaeb52ae
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-06-29 17:51:09 +0000

    feat: create function calculate and backSpace

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,24 @@
-let result = document.querySelector('.result').textContent;
-function insert(num){   
-    let number = document.querySelector('.result').innerHTML;
-    document.querySelector('.result').innerHTML = number + num;
+let result = document.querySelector(".result").textContent;
+function insert(num) {
+  let number = document.querySelector(".result").innerHTML;
+  document.querySelector(".result").innerHTML = number + num;
 }
 
-function clean(){
+function clean() {
+  document.querySelector(".result").innerHTML = "";
+}
 
-document.querySelector('.result').innerHTML = "";
+function backSpace() {
+  let result = document.querySelector(".result").innerHTML;
+  document.querySelector(".result").innerHTML = result.substring(0, result.length - 1);
+}
 
+function calculate() {
+  let result = document.querySelector(".result").innerHTML;
+
+  if (result) {
+    document.querySelector(".result").innerHTML = eval(result);
+  } else {
+    document.querySelector(".result").innerHTML = "número inválido";
+  }
 }
